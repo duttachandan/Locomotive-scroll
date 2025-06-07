3a93f5a04acdae3376a84f3d69b717c2910162c9
Chandan Dutta <duttaankit917@gmail.com>
2025-06-08 00:29:00 +0530

    update images animation

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+    smoothMobile: true,
+});
+
+
+
+// lerp: 0.1,
+// multiplier: 1.2,
+// tablet: { smooth: true },
+// smartphone: { smooth: false }
