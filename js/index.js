c8bbad4f7a2be36efbed6ba406f8e0637d16c4be
duttachandan <duttaankit917@gmail.com>
2025-06-10 12:26:51 +0530

    updated lerp

--- js/index.js
+++ js/index.js
@@ -1,20 +1,14 @@
-
 AOS.init();
 
-
 document.addEventListener("DOMContentLoaded", () => {
-    const scroll = new LocomotiveScroll({
-        el: document.querySelector("[data-scroll-container]"),
-        smooth: true,
-        smoothMobile: true,
-        multiplier: 1.2,
-        tablet: { smooth: true },
-        smartphone: { smooth: true }
-    });
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+    smoothMobile: true,
+    tablet: { smooth: true },
+    smartphone: { smooth: true },
+  });
 });
-
-
-
 
 // lerp: 0.1,
 // multiplier: 1.2,
